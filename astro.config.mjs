550184b6158a6c4e608aae2926cb51de3770f21d
wing-0902 <wing@me.wing.osaka>
2025-08-29 12:40:14 +0000

    依存関係をインストール

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  output: 'static',
+  integrations: [
+    svelte(),
+    sitemap()
+  ],
 });
