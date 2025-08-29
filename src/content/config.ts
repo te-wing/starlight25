2c1cc30d1834f0976e4b4efcd8f1f2d809b3e845
wing-0902 <wing@me.wing.osaka>
2025-08-29 13:49:03 +0000

    整数ってむずいね．

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,7 @@ const articleCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    publishDate: z.string().transform(str => new Date(str)), // 日付をDateオブジェクトに変換
+    pagenum: z.number().int(),
     author: z.string().default('Unknown'), // デフォルト値の設定
     tags: z.array(z.string()).optional(), // オプショナルな配列
     kind: z.string(), // 種類（本編，活動報告，ラフ版？）
