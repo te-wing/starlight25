ab37324f2d2c52f3b0081a3ac49124a28ad8d677
wing-0902 <wing@me.wing.osaka>
2025-09-05 11:57:28 +0000

    メッセージは作った

--- src/components/FormsFunc/FormUI.svelte
+++ src/components/FormsFunc/FormUI.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
+  import RequiredMark from './RequiredMark.svelte';
 
+  let about: string = 'undefined';
+
+  export let aboutThis;
+  
+  if (aboutThis === 'book') {
+    about = '部誌';
+  } else if (aboutThis === 'planetarium') {
+    about = 'プラネタリウム';
+  }
 </script>
 
 <div>
+  <h2>{about}についてのアンケート</h2>
+  <p>天文部の部誌「The Scientific Seiko」について，ご意見をお聞かせください．</p>
+  <p>「<RequiredMark />」がついている項目は，回答必須です．</p>
+
+  <form>
+
+  </form>
 </div>
 
 <style lang='scss'>
---
+++ src/components/FormsFunc/RequiredMark.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<span>
+  *
+</span>
+
+<style lang='scss'>
+  span {
+    color: red;
+  }
+</style>
