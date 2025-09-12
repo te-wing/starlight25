3b673cf2fe960d58c1d9e1e72418da7fbcd2c1ba
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-09-12 13:02:36 +0000

    login

---
+++ src/components/admin/LoginForm.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { signInWithEmailAndPassword } from 'firebase/auth';
+  import { auth } from '../../firebase/client';
+
+  let email = '';
+  let password = '';
+  let errorMessage = '';
+
+  const handleAuth = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      window.location.href = '/dash/loginComplete';
+    } catch (error: any) {
+      errorMessage = error.message;
+      alert(`エラー: ${errorMessage}`);
+      console.error(error);
+    }
+  };
+</script>
+
+<div>
+  <h3>管理者ログイン</h3>
+  <section>
+    <form on:submit|preventDefault={() => handleAuth()}>
+      <label for="email">メールアドレス</label>
+      <input type="email" bind:value={email} required /><br />
+      <label for="password">パスワード</label>
+      <input type="password" bind:value={password} required minlength="6" /><br />
+      {#if errorMessage}
+        <p>{errorMessage}</p>
+      {/if}
+      <button type="submit">ログイン</button>
+    </form>
+  </section>
+</div>
+
+<style lang='scss'>
+
+</style>
