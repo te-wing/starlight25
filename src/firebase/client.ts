1f4e4736a5ab42d84f802f71054dadbf1a1ad82b
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-09-12 12:53:18 +0000

    auth

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { getAuth } from "firebase/auth";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC-S2FT3LKvChkKe6Yvqcm24P2pDGwWQX4",
@@ -15,3 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth };
