0822aeda512d28315e18348865c8d7a88614bb43
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-09-12 07:24:06 +0000

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  adapter: cloudflare(),
   integrations: [svelte(), sitemap(), react()],
 });
