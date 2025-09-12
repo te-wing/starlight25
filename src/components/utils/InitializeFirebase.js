31d77bc17c1040b67408fb7d81a02c5635a85f47
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-09-12 12:40:11 +0000

    Initialize

---
+++ src/components/utils/InitializeFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-S2FT3LKvChkKe6Yvqcm24P2pDGwWQX4",
+  authDomain: "sf25-tenmon.firebaseapp.com",
+  projectId: "sf25-tenmon",
+  storageBucket: "sf25-tenmon.firebasestorage.app",
+  messagingSenderId: "849951922230",
+  appId: "1:849951922230:web:1d30292956eebecb102097"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
