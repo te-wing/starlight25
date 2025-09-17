a7f0b804f9f115da4feab0481871fd19f3f7d768
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-09-17 13:05:26 +0000

    input

--- src/components/admin/TicketsFunc/AddTimeBox.svelte
+++ src/components/admin/TicketsFunc/AddTimeBox.svelte
@@ -3,9 +3,15 @@
 </script>
 
 <div>
-
+  <form>
+    <input class='timeInput' type='time' placeholder="22:02">
+  </form>
 </div>
 
 <style lang="scss">
-
+  .timeInput {
+    font-family: ZenMaru;
+    font-size: 1em;
+    margin: 3px 15px;
+  }
 </style>
