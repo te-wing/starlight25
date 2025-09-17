1b18f4d4559ad7cbd57248d00938e5751f7b110b
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-09-17 13:00:22 +0000

    addTIme

---
+++ src/components/admin/TicketsFunc/TicketList.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { db } from '../../../firebase/client.ts';
+  import { ref, onValue } from 'firebase/database';
+  import { writable } from 'svelte/store';
+  import type { Unsubscriber } from 'svelte/store';
+
+
+</script>
+
+<div>
+  <a href='/admin/ticket/addTime'>時間帯を追加</a>
+</div>
