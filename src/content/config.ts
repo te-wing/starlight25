fa9104bf5c653b588897484654c899b23fbd0d35
Wing <114337707+wing-0902@users.noreply.github.com>
2025-10-23 15:19:04 +0000

    まあ今日はこんなもんでええか？

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+// 1. `astro:content`からユーティリティをインポート
+import { z, defineCollection } from 'astro:content';
+// 2. コレクションを定義
+const bookCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    authorInitial: z.string(),
+    kind: z.string(),
+    pageNo: z.string(),
+  }),
+});
+// 3. コレクションを登録するために、単一の`collections`オブジェクトをエクスポート
+//    このキーは、"src/content"のコレクションのディレクトリ名と一致する必要があります。
+export const collections = {
+  'book': bookCollection,
+};
