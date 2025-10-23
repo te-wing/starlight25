8368178d370b20d518063081071ba409612a1153
Wing <114337707+wing-0902@users.noreply.github.com>
2025-10-23 12:21:41 +0000

    OK

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,10 @@ import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [svelte(), sitemap(), react()],
+  integrations: [svelte(), sitemap(), react(), vue()],
 });
