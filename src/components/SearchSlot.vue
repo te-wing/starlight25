3fcca40a5f0358a3aee3fdb46e03e89124aac401
Wing <114337707+wing-0902@users.noreply.github.com>
2025-10-24 22:07:07 +0000

    これでいい？

--- src/components/SearchSlot.vue
+++ src/components/SearchSlot.vue
@@ -23,10 +23,9 @@
   .searchBox {
     .search {
       --pagefind-ui-scale: 1;
-      --pagefind-ui-primary: #034ad8;
       --pagefind-ui-text: var(--foreground);
       --pagefind-ui-background: transparent;
-      --pagefind-ui-border: var(--button-hover-border);
+      --pagefind-ui-border: var(--foreground);
       --pagefind-ui-tag: var(--foregroundSub);
       --pagefind-ui-border-width: 2px;
       --pagefind-ui-border-radius: 8px;
